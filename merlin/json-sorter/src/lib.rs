2c3b8ec2086d75dbeeba721974805de174a74dc4
wtlin1228 <wtlin1228@gmail.com>
2024-03-01 22:12:42 +0800

    merlin created a new lib for sorting JSON

---
+++ merlin/json-sorter/src/lib.rs
@@ -0,0 +1,104 @@
+#![warn(missing_docs, missing_debug_implementations)]
+
+//! # JSON Sorter Library
+//!
+//! The JSON Sorter library provides functionality to read a JSON file, sort its contents by keys,
+//! and write the sorted JSON back to a file.
+//!
+//! ## Usage
+//!
+//! ```rust no_run
+//! use json_sorter::JsonSorter;
+//!
+//! fn main() {
+//!     // Example usage
+//!     let file_path = "path/to/your/json/file.json";
+//!     let mut sorter = JsonSorter::from(file_path).unwrap();
+//!     sorter.sort_contents().unwrap();
+//!     sorter.write_to_file("sorted_file.json").unwrap();
+//! }
+//! ```
+
+use std::{
+    error::Error,
+    fs::{self, File},
+    io::{BufRead, BufReader, Write},
+};
+
+/// Alias for the error type returned by JsonSorter methods.
+type JsonSorterError = Box<dyn Error + Send + Sync + 'static>;
+
+/// Represents a key-value pair in the JSON Sorter.
+#[derive(Debug)]
+struct KeyValuePair {
+    key: String,
+    value: String,
+}
+
+/// Represents a JSON Sorter instance.
+#[derive(Debug)]
+pub struct JsonSorter {
+    contents: Vec<KeyValuePair>,
+}
+
+impl JsonSorter {
+    /// Constructs a new JsonSorter instance from the JSON file located at the given `file_path`.
+    pub fn from(file_path: &str) -> Result<Self, JsonSorterError> {
+        let mut json_sorter = JsonSorter { contents: vec![] };
+        let mut file = File::open(file_path)?;
+        let reader = BufReader::new(&mut file);
+        for line in reader.lines() {
+            let line = line?;
+            let line = line.trim();
+            match line {
+                s if s.is_empty() || s == "{" || s == "}" => (),
+                _ => {
+                    let (key, value) = line
+                        .split_once(":")
+                        .expect("a valid translation key-value pair should have at least one `:`");
+                    let value = value.trim();
+                    json_sorter.contents.push(KeyValuePair {
+                        key: key[1..key.len() - 1].to_owned(), // "key" => key
+                        value: match &value[value.len() - 1..] {
+                            "," => value[1..value.len() - 2].to_owned(), // "value", => value
+                            _ => value[1..value.len() - 1].to_owned(),   // "value" => value
+                        },
+                    });
+                }
+            }
+        }
+        Ok(json_sorter)
+    }
+
+    /// Sorts the contents of the JSON Sorter by keys.
+    pub fn sort_contents(&mut self) -> Result<(), JsonSorterError> {
+        self.contents.sort_by(|a, b| {
+            let a = &a.key[..];
+            let b = &b.key[..];
+            a.cmp(b)
+        });
+        Ok(())
+    }
+
+    /// Writes the contents of the JSON Sorter to a file located at the given `file_path`.
+    pub fn write_to_file(&self, file_path: &str) -> Result<(), JsonSorterError> {
+        let _ = fs::remove_file(file_path);
+        let mut file = File::create_new(file_path)?;
+        file.write_all(b"{\n")?;
+        for (i, single_translation) in self.contents.iter().enumerate() {
+            let line = format!(
+                "    \"{}\": \"{}\"{}",
+                single_translation.key,
+                single_translation.value,
+                if i != self.contents.len() - 1 {
+                    ",\n"
+                } else {
+                    "\n"
+                }
+            );
+            file.write_all(line.as_bytes())?;
+        }
+        file.write_all(b"}\n")?;
+        Ok(())
+    }
+}
