6d748890ce1ad49f06ff8f9ac40d6153ce000210
wtlin1228 <wtlin1228@gmail.com>
2024-03-04 02:29:43 +0800

    Merlin added some tests for DependencyVisitor

--- merlin/dependency-tracker/tests/track_id_visitor.rs
+++ merlin/dependency-tracker/tests/track_id_visitor.rs
@@ -1,4 +1,6 @@
+use dependency_tracker::visitors::track_id_visitor::TrackIdVisitor;
 use std::collections::HashSet;
+
 use swc_core::{
     common::{
         errors::{ColorConfig, Handler},
@@ -12,50 +14,48 @@ use swc_core::{
 };
 use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax, TsConfig};
 
-use dependency_tracker::visitors::track_id_visitor::TrackIdVisitor;
-
 macro_rules! parse_with_visitors {
     ($input:expr, $($visitor:expr),*) => {
-      let cm: Lrc<SourceMap> = Default::default();
-      let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
+        let cm: Lrc<SourceMap> = Default::default();
+        let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
 
-      let fm = cm.new_source_file(FileName::Custom("test.js".into()), $input.into());
+        let fm = cm.new_source_file(FileName::Custom("test.js".into()), $input.into());
 
-      let lexer = Lexer::new(
-          Syntax::Typescript(TsConfig {
-              tsx: true,
-              decorators: false,
-              dts: false,
-              no_early_errors: true,
-              disallow_ambiguous_jsx_like: true,
-          }),
-          Default::default(),
-          StringInput::from(&*fm),
-          None,
-      );
+        let lexer = Lexer::new(
+            Syntax::Typescript(TsConfig {
+                tsx: true,
+                decorators: false,
+                dts: false,
+                no_early_errors: true,
+                disallow_ambiguous_jsx_like: true,
+            }),
+            Default::default(),
+            StringInput::from(&*fm),
+            None,
+        );
 
-      let mut parser = Parser::new_from(lexer);
+        let mut parser = Parser::new_from(lexer);
 
-      for e in parser.take_errors() {
-          e.into_diagnostic(&handler).emit();
-      }
+        for e in parser.take_errors() {
+            e.into_diagnostic(&handler).emit();
+        }
 
-      let module = parser
-          .parse_module()
-          .map_err(|e| {
-              // Unrecoverable fatal error occurred
-              e.into_diagnostic(&handler).emit()
-          })
-          .expect("failed to parser module");
+        let module = parser
+            .parse_module()
+            .map_err(|e| {
+                // Unrecoverable fatal error occurred
+                e.into_diagnostic(&handler).emit()
+            })
+            .expect("failed to parser module");
 
-      GLOBALS.set(&Globals::new(), || {
-          let module = module.fold_with(&mut resolver(Mark::new(), Mark::new(), true));
-          $(module.visit_with(&mut $visitor);)*
-      });
+        GLOBALS.set(&Globals::new(), || {
+            let module = module.fold_with(&mut resolver(Mark::new(), Mark::new(), true));
+            $(module.visit_with(&mut $visitor);)*
+        });
     };
 
     ($input:expr, $($visitor:expr,)*) => {
-      $crate::parse_with_visitors![$input:expr, $($visitor),*]
+        $crate::parse_with_visitors![$input:expr, $($visitor),*]
     };
 }
 
