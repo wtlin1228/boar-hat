3a02456a8117fa19aee6fb4d3885d268bb62cc85
wtlin1228 <wtlin1228@gmail.com>
2024-03-16 15:01:30 +0800

    Merlin added CLI for i18n tool

--- merlin/i18n/src/main.rs
+++ merlin/i18n/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow;
+use clap::Parser;
 use i18n::{extract_i18n_keys::extract_i18n_keys, GitDiffStatusLetter};
 use rand::{distributions::Alphanumeric, Rng};
 use std::{
@@ -8,29 +9,41 @@ use std::{
     process::Command,
 };
 
-fn should_track(status_letter: &str, file_path: &str) -> bool {
-    GitDiffStatusLetter::should_track(status_letter)
-        && (file_path.ends_with(".js")
-            || file_path.ends_with(".ts")
-            || file_path.ends_with(".jsx")
-            || file_path.ends_with(".tsx"))
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Absolute path for your repository
+    #[arg(short, long)]
+    repo_path: String,
+
+    /// The first commit for git diff <commit> <commit>
+    #[arg(short, long)]
+    commit1: String,
+
+    /// The second commit for git diff <commit> <commit>
+    #[arg(short = 'd', long, default_value_t = String::from("HEAD"))]
+    commit2: String,
+
+    /// Include the file names where keys extracted from
+    #[arg(short, long, default_value_t = false)]
+    include_file_name: bool,
 }
 
 fn main() -> anyhow::Result<()> {
-    // 1. get the changed file paths (source files)
-    // 2. extract i18n keys from source files
-    // 3. append extracted i18n keys to target files (e.g. en.json)
-    // 4. (manually) fill in the new keys
-    // 5. sort the target files
+    let cli = Cli::parse();
+    let repo_path = cli.repo_path;
+    let commit1 = cli.commit1;
+    let commit2 = cli.commit2;
+    let should_output_file_name = cli.include_file_name;
 
-    let repo_path = "/Users/wtlin1228/iCHEF/Napoleon/napoleon/frontend";
+    // git diff --name-status <commit> <commit>
     let mut git = Command::new("git");
     let output = git
-        .current_dir(repo_path)
+        .current_dir(&repo_path)
         .arg("diff")
         .arg("--name-status")
-        .arg("develop")
-        .arg("HEAD")
+        .arg(commit1)
+        .arg(commit2)
         .output()
         .expect("git diff failed");
 
@@ -45,20 +58,15 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    let random_string: String = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(7)
-        .map(char::from)
-        .collect();
-    let out_file_name = format!("i18n-keys.{}.jsonc", random_string);
-    let out_file_path = Path::new(repo_path).join(out_file_name);
+    let out_file_name = generate_output_file_name();
+    let out_file_path = Path::new(&repo_path).join(out_file_name);
     let file = File::create(out_file_path)?;
     let mut stream = BufWriter::new(file);
 
     stream.write(b"{\n")?;
     for source_file in source_files {
         let extracted_keys = extract_i18n_keys(&format!("{}/{}", repo_path, source_file));
-        if extracted_keys.len() > 0 {
+        if extracted_keys.len() > 0 && should_output_file_name {
             stream.write(format!("\t// {}\n", source_file).as_bytes())?;
         }
         for t in extracted_keys {
@@ -68,4 +76,21 @@ fn main() -> anyhow::Result<()> {
     stream.write(b"}\n")?;
 
     anyhow::Ok(())
+}
+
+fn should_track(status_letter: &str, file_path: &str) -> bool {
+    GitDiffStatusLetter::should_track(status_letter)
+        && (file_path.ends_with(".js")
+            || file_path.ends_with(".ts")
+            || file_path.ends_with(".jsx")
+            || file_path.ends_with(".tsx"))
+}
+
+fn generate_output_file_name() -> String {
+    let random_string: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(7)
+        .map(char::from)
+        .collect();
+    format!("i18n-keys.{}.jsonc", random_string)
 }
