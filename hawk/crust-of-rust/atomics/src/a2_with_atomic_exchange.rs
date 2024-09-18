ac1f42ce9f0df545e5b0d5da74c3b6548072294e
wtlin1228 <wtlin1228@gmail.com>
2024-09-18 10:15:02 +0800

    Hawk added MESI protocol

--- hawk/crust-of-rust/atomics/src/a2_with_atomic_exchange.rs
+++ hawk/crust-of-rust/atomics/src/a2_with_atomic_exchange.rs
@@ -22,11 +22,20 @@ impl<T> Mutex<T> {
     }
 
     pub fn with_lock<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+        // Performance problem:
+        // compare_exchange is an expensive operation since OS need to coordinate
+        // between all the CPUs to get the exclusive access of the specific memory
         while self
             .locked
             .compare_exchange(UNLOCKED, LOCKED, Ordering::Relaxed, Ordering::Relaxed)
             .is_err()
-        {}
+        {
+            // MESI protocol is used to solve the performance problem of
+            // compare_exchange, it uses a inner loop to read the lock.
+            // So, if we failed the compare_exchange, we just spining here
+            // until the lock is unlicked then try to compare_exchange again.
+            while self.locked.load(Ordering::Relaxed) == LOCKED {}
+        }
         // 🔓 no another thread can run here simutanuously
         // 👇 use yield_now() to confirm this situation
         std::thread::yield_now();
