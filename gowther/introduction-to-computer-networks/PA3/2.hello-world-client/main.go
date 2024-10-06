41fcb2d3f84dfbf6917033b786e04ef1e913b352
wtlin1228 <wtlin1228@gmail.com>
2024-10-07 00:45:31 +0800

    Gowther finished PA3 for networking

---
+++ gowther/introduction-to-computer-networks/PA3/2.hello-world-client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func main() {
+	conn, errc := net.Dial("tcp", "127.0.0.1:8080")
+	check(errc)
+	defer conn.Close()
+	writer := bufio.NewWriter(conn)
+	len, errw := writer.WriteString("Hello World!\n")
+	check(errw)
+	fmt.Printf("Send a string of %d bytes\n", len)
+	writer.Flush()
+	scanner := bufio.NewScanner(conn)
+	if scanner.Scan() {
+		fmt.Printf("Server replies: %s\n", scanner.Text())
+	}
+}
--- gowther/introduction-to-computer-networks/README.md
+++ gowther/introduction-to-computer-networks/README.md
@@ -1,3 +1,4 @@
 # Introduction to Computer Networks
 
 Course link: https://homepage.ntu.edu.tw/~pollyhuang/teach/intro-cn-spring-24/
+Video link: https://www.youtube.com/playlist?list=PLYnSKmvu-glJBg3nIflyUkh0RILYVVSS8
