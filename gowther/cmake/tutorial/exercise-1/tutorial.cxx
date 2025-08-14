c106839b52a49c060ae64feea78fe055b4c7bc0e
wtlin1228 <wtlin1228@gmail.com>
2025-08-14 18:06:40 +0900

    Gowther finished cmake exercise 1

--- gowther/cmake/tutorial/exercise-1/tutorial.cxx
+++ gowther/cmake/tutorial/exercise-1/tutorial.cxx
@@ -1,23 +1,22 @@
 // A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib> // TODO 5: Remove this line
 #include <iostream>
 #include <string>
 
-// TODO 11: Include TutorialConfig.h
+#include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
-    // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
-    //          and Tutorial_VERSION_MINOR
+    // report version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
 
   // convert input to double
-  // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
-  double const inputValue = atof(argv[1]);
+  double const inputValue = std::stod(argv[1]);
 
   // calculate square root
   double const outputValue = sqrt(inputValue);
