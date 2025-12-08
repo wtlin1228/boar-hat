b9260b5cabd5251f5f96a9e5c99ac1dc8dbfbed2
wtlin1228 <wtlin1228@gmail.com>
2025-12-08 09:41:28 +0900

    Gowther passed distrubuted system lab2.2

--- gowther/distributed-systems/labs/src/kvsrv1/lock/lock.go
+++ gowther/distributed-systems/labs/src/kvsrv1/lock/lock.go
@@ -1,6 +1,9 @@
 package lock
 
 import (
+	"time"
+
+	"6.5840/kvsrv1/rpc"
 	kvtest "6.5840/kvtest1"
 )
 
@@ -11,6 +14,9 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	id          string
+	l           string
+	lockVersion rpc.Tversion
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +25,42 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
 	// You may add code here
+	id := kvtest.RandValue(8)
+	lk := &Lock{ck: ck, id: id, l: l}
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		lockState, version, err := lk.ck.Get(lk.l)
+		switch err {
+		case rpc.ErrNoKey:
+			err := lk.ck.Put(lk.l, lk.id, 0)
+			if err == rpc.OK {
+				lk.lockVersion = 1
+				return
+			}
+		case rpc.OK:
+			if lockState == "unlock" {
+				err := lk.ck.Put(lk.l, lk.id, version)
+				lk.lockVersion = version + 1
+				if err == rpc.OK {
+					return
+				}
+			}
+		default:
+			// ignore other cases
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	lockState, version, err := lk.ck.Get(lk.l)
+	if err == rpc.OK && lockState == lk.id && version == lk.lockVersion {
+		lk.ck.Put(lk.l, "unlock", lk.lockVersion)
+	}
 }
